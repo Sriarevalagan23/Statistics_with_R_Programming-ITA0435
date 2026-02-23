dc1a44ea2bdd95b9961eb78430628502ee70f96f
Srinidhi A <158122890+Sriarevalagan23@users.noreply.github.com>
2026-02-23 13:44:39 +0530

    Add files via upload

---
+++ exp11.R
@@ -0,0 +1,3 @@
+data <- read.csv("exam_data.csv")
+print("Contents of the CSV file:")
+print(data)
---
+++ exp20.R
@@ -0,0 +1,5 @@
+data("ChickWeight")
+
+m <- lm(weight ~ Time + Diet, data = ChickWeight)
+p <- predict(m)
+mean((ChickWeight$weight - p)^2)
